ac6780ecd09b79650e5cc8247f9ae98a127a76c7
CHOSEONGJUN <lionjojo@naver.com>
2024-09-05 17:46:02 +0900

    feat: Add loop feature / refactor : Exception Handling 사용자가 원할떄때까지 루프 기능 추가, 예외처리 구문 다중 catch 로 변경

--- src/com/calculator/lv1/Calculator.java
+++ src/com/calculator/lv1/Calculator.java
@@ -11,51 +11,70 @@ public class Calculator {
         int firstNum = 0;
         int secondNum = 0;
         double result = 0;
-        // 숫자가 아닌 문자를 입력시 예외 발생 후 프로그램 종료
-        try {
-            // 스캐너를 통한 숫자 1,2 입력 후 변수저장.
-            System.out.print("첫 번째 숫자를 입력하세요 : ");
-            firstNum = sc.nextInt();
-            System.out.print("두 번째 숫자를 입력하세요 : ");
-            secondNum = sc.nextInt();
-        } catch (InputMismatchException e) {
-            System.out.println("숫자를 입력해주세요.");
-            System.exit(0);
-        }
-        // 스캐너를 통한 사칙연산 기호 삽입.
-        System.out.print("사칙연산 기호를 입력하세요 : ");
-        char operator = sc.next().charAt(0);
-        // 0으로 나누기를 실행할경우 예외 발생
-        try {
-            // 연산자의 따른 연산 진행 제어문
-            switch (operator) {
-                case '+':
-                    result = firstNum + secondNum;
-                    System.out.println(firstNum + " " + operator + " " + secondNum + " = " + result);
-                    break;
-                case '-':
-                    result = firstNum - secondNum;
-                    System.out.println(firstNum + " " + operator + " " + secondNum + " = " + result);
-                    break;
-                case '*':
-                    result = firstNum * secondNum;
-                    System.out.println(firstNum + " " + operator + " " + secondNum + " = " + result);
-                    break;
-                case '/':
-                    result = firstNum / secondNum;
-                    System.out.println(firstNum + " " + operator + " " + secondNum + " = " + result);
-                    break;
-                case '%':
-                    result = firstNum % secondNum;
-                    System.out.println(firstNum + " " + operator + " " + secondNum + " = " + result);
-                    break;
-                default:
-                    System.out.println("존재하지 않는 연산자 입니다.");
-                    break;
+        boolean flag = true;
+        boolean onOfflag;
+        // 계산기 무한반복 진행.
+        while (flag) {
+            try {
+                onOfflag = true;
+                // 스캐너를 통한 숫자 1,2 입력 후 변수저장.
+                System.out.print("첫 번째 숫자를 입력하세요 : ");
+                firstNum = sc.nextInt();
+                System.out.print("두 번째 숫자를 입력하세요 : ");
+                secondNum = sc.nextInt();
+                // 스캐너를 통한 사칙연산 기호 삽입.
+                System.out.print("사칙연산 기호를 입력하세요 : ");
+                char operator = sc.next().charAt(0);
+                // 연산자의 따른 연산 진행 제어문
+                switch (operator) {
+                    case '+':
+                        result = firstNum + secondNum;
+                        System.out.println(firstNum + " " + operator + " " + secondNum + " = " + result);
+                        break;
+                    case '-':
+                        result = firstNum - secondNum;
+                        System.out.println(firstNum + " " + operator + " " + secondNum + " = " + result);
+                        break;
+                    case '*':
+                        result = firstNum * secondNum;
+                        System.out.println(firstNum + " " + operator + " " + secondNum + " = " + result);
+                        break;
+                    case '/':
+                        result = firstNum / secondNum;
+                        System.out.println(firstNum + " " + operator + " " + secondNum + " = " + result);
+                        break;
+                    case '%':
+                        result = firstNum % secondNum;
+                        System.out.println(firstNum + " " + operator + " " + secondNum + " = " + result);
+                        break;
+                    default:
+                        System.out.println("존재하지 않는 연산자 입니다.");
+                        break;
+                }
+                // yes 와 exit 외의 문자가 입력될 경우 다시 반복.
+                while (onOfflag) {
+                    System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)(yes 입력 시 계속)");
+                    String onOff = sc.next();
+                    // exit 가 입력될경우 프로그램 종료
+                    if (onOff.equals("exit")) {
+                        flag = !flag;
+                        onOfflag = !onOfflag;
+                        // yes 입력시 계산 재진행.
+                    } else if (onOff.equals("yes")) {
+                        System.out.println("계산을 다시 진행합니다.");
+                        onOfflag = !onOfflag;
+                    } else {
+                        System.out.println("yes 혹은 exit 로 만 입력해주세요.");
+                    }
+                }
+                // 0 으로 나누기 연산시 예외발생 처리
+            } catch (ArithmeticException e) {
+                System.out.println("0으로는 나눌 수 없습니다.");
+                // 숫자 실수 및 문자 입력시 예외발생 처리
+            } catch (InputMismatchException e) {
+                System.out.println("숫자 혹은 정수를 입력해주세요.");
+                sc.nextLine();
             }
-        } catch (ArithmeticException e) {
-            System.out.println("0으로는 나눌 수 없습니다.");
         }
-
     }
 }
