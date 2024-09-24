4e8d0a3a0c821c582ec20aa785da261179e03236
CHOSEONGJUN <lionjojo@naver.com>
2024-09-24 09:44:34 +0900

    refactor: Change parameter String to AbstractCalculator -계산 연산자를 스트링이 아닌 추상클래스타입을 받아 연산

--- src/com/calculator/lv3/operations/AbstractCalculator.java
+++ src/com/calculator/lv3/operations/AbstractCalculator.java
@@ -1,6 +1,10 @@
-package com.calculator.lv3;
+package com.calculator.lv3.operations;
 
 // 계산기 기능 class들이 구현할 공유목적의 추상클래스 선언
 public abstract class AbstractCalculator<T extends Number> {
+    private String operator;
+
     public abstract double calculate(T firstNumber, T secondNumber);
+
+    public abstract String getOperator();
 }
--- src/com/calculator/lv3/operations/SubCalculator.java
+++ src/com/calculator/lv3/operations/SubCalculator.java
@@ -1,9 +1,13 @@
-package com.calculator.lv3;
+package com.calculator.lv3.operations;
 
 // 추상클래스를 상속받아 오버라이딩 하여 기능 구현.
 public class SubCalculator<T extends Number> extends AbstractCalculator {
     @Override
     public double calculate(Number firstNumber, Number secondNumber) {
         return firstNumber.doubleValue() - secondNumber.doubleValue();
+    }
+
+    public String getOperator() {
+        return "-";
     }
 }
